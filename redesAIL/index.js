97a4b6b715e317000d189a1a18821872f1a799be
iagosouza12-cloud <iago.souza12@aluno.ifce.edu.br>
2026-09-03 16:33:23 -0300

    Add files via upload

---
+++ redesAIL/index.js
@@ -0,0 +1,7 @@
+alert("Eu sou o melhor vaqueiro do Brasil!!!!")
+
+button = document.querySelector("button")
+
+button.addEventListener("click", function(){
+    alert("você acabou de conhecer o melhor vaqueiro do brasil")
+})
